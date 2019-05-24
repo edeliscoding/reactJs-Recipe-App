8a29e10a964364cb0df1984d03fa5bd69b7bc125
edeliscoding <edelreichd@gmail.com>
2019-05-23 19:50:44 -0600

    first committ recipe

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return <div>hello from our recipe app</div>;
+  }
+}
+
+export default App;
